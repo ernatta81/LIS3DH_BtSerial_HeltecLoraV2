da902d8dfdb5f2cb1e0a7ad63a5961e81bcea421
ernatta81 <ernattamaker@gmail.com>
2024-11-14 16:14:47 +0100

    Aggiunto setup tramite serialBT

--- src/main.cpp
+++ src/main.cpp
@@ -10,12 +10,18 @@ Adafruit_LIS3DH lis = Adafruit_LIS3DH();
 #define SDA_PIN 21
 #define SCL_PIN 22
 
-const char *pin = "0000";// Definisci il PIN
-bool startReceived = false; // Variabile per  "START"
+const char *pin = "0000"; // Bluetooth il PIN
+bool startReceived = false; // Variabile per "START"
+bool configureAccelerometer = false;
+
+void configureAccelerometerSettings(); // Dichiarazione della funzione altrimenti non compila....
+void configureLISPerformance();
+void configureLISRange();
+void configureLISDatarate();
+void printLIS3Configuration();
 
 void setup(void) {
   SerialPortBt.begin("V2LIS3DH");
-  //SerialPortBt.setPin(pin); // Abilita il PIN
   Serial.begin(115200);
   Wire.begin(SDA_PIN, SCL_PIN);
 
@@ -29,54 +35,66 @@ void setup(void) {
   }
   Serial.println("Bluetooth connesso!");
 
-  if (!lis.begin(0x19)) {
-    Serial.println("LIS3DH not start");
-    SerialPortBt.println("LIS3DH not start");
-    while (1) yield();
+  Serial.println("Vuoi configurare l'accelerometro? (S/N)");
+  SerialPortBt.println("Vuoi configurare l'accelerometro? (S/N)");
+  while (true) {
+    if (SerialPortBt.available()) {
+      char response = SerialPortBt.read();
+      if (response == 'S' || response == 's') {
+        configureAccelerometer = true;
+        Serial.println("Configurazione accelerometro...");
+        SerialPortBt.println("Configurazione accelerometro...");
+        configureAccelerometerSettings(); // Funzione per la configurazione
+        printLIS3Configuration();
+        break;
+      } else if (response == 'N' || response == 'n') {
+        Serial.println("Skipping configuration. Waiting for START command...");
+        SerialPortBt.println("Skipping configuration. Waiting for START command...");
+        printLIS3Configuration();
+        
+        break;
+      }
+    }
   }
-  Serial.println("LIS3DH found!");
 
-  // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 o 16 G!
-  Serial.print("Range = "); Serial.print(2 << lis.getRange());
-  Serial.println("G");
-
-  // lis.setPerformanceMode(LIS3DH_MODE_LOW_POWER);
-  Serial.print("Performance mode set to: ");
-  switch (lis.getPerformanceMode()) {
-    case LIS3DH_MODE_NORMAL: Serial.println("Normal 10bit"); break;
-    case LIS3DH_MODE_LOW_POWER: Serial.println("Low Power 8bit"); break;
-    case LIS3DH_MODE_HIGH_RESOLUTION: Serial.println("High Resolution 12bit"); break;
+  if (!configureAccelerometer) {
+    if (!lis.begin(0x19)) {
+      Serial.println("LIS3DH not start");
+      SerialPortBt.println("LIS3DH not start");
+      while (1) yield();
+    }
+    Serial.println("LIS3DH found!");
   }
-
-  // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
-  Serial.print("Data rate set to: ");
-  switch (lis.getDataRate()) {
-    case LIS3DH_DATARATE_1_HZ: Serial.println("1 Hz"); break;
-    case LIS3DH_DATARATE_10_HZ: Serial.println("10 Hz"); break;
-    case LIS3DH_DATARATE_25_HZ: Serial.println("25 Hz"); break;
-    case LIS3DH_DATARATE_50_HZ: Serial.println("50 Hz"); break;
-    case LIS3DH_DATARATE_100_HZ: Serial.println("100 Hz"); break;
-    case LIS3DH_DATARATE_200_HZ: Serial.println("200 Hz"); break;
-    case LIS3DH_DATARATE_400_HZ: Serial.println("400 Hz"); break;
-    case LIS3DH_DATARATE_POWERDOWN: Serial.println("Powered Down"); break;
-    case LIS3DH_DATARATE_LOWPOWER_5KHZ: Serial.println("5 Khz Low Power"); break;
-    case LIS3DH_DATARATE_LOWPOWER_1K6HZ: Serial.println("1.6 Khz Low Power"); break;
-  }
+  Serial.println("Waiting receive START command");
+  SerialPortBt.println("Waiting receive START command");
 }
+
+void configureAccelerometerSettings() {
+  configureLISPerformance();
+  configureLISRange();
+  configureLISDatarate();
+}
+  
 
 void loop() {
   if (SerialPortBt.available()) {
     String command = SerialPortBt.readStringUntil('\n');  // Leggi il comando ricevuto
     if (command == "START") {
-      startReceived = true; //  "START"
+      startReceived = true;  // "START"
       Serial.println("Received START command. Beginning sensor data output...");
       SerialPortBt.println("Received START command. Beginning sensor data output...");
+    } 
+    else if (command == "STOP") {
+      startReceived = false;  // "STOP"
+      Serial.println("Received STOP command. Stopping sensor data output...");
+      SerialPortBt.println("Received STOP command. Stopping sensor data output...");
     }
   }
 
   if (startReceived) {
-    lis.read();  // ottieni i dati X Y e Z
-    // dati grezzi
+    lis.read();  // Ottieni i dati X Y e Z
+
+    // Dati grezzi
     Serial.print("X:  "); Serial.print(lis.x);
     Serial.print("  \tY:  "); Serial.print(lis.y);
     Serial.print("  \tZ:  "); Serial.print(lis.z);
@@ -86,7 +104,7 @@ void loop() {
     SerialPortBt.print("  \tY:  "); SerialPortBt.print(lis.y);
     SerialPortBt.print("  \tZ:  "); SerialPortBt.print(lis.z);
 
-    /* ottieni un nuovo evento dal sensore, normalizzato */
+    /* Ottieni un nuovo evento dal sensore, normalizzato */
     sensors_event_t event;
     lis.getEvent(&event);
 
@@ -105,4 +123,200 @@ void loop() {
   }
 
   delay(200);
+}
+
+void configureLISRange(){
+  Serial.println("Set range to 2G, 4G, 8G or 16G?");
+  SerialPortBt.println("Set range to 2G, 4G, 8G or 16G?");
+   while (true) {
+    if (SerialPortBt.available()) {
+      String range = SerialPortBt.readStringUntil('\n');
+      if (range == "2G") {
+        lis.setRange(LIS3DH_RANGE_2_G);
+        Serial.println("Range set to 2G");
+        SerialPortBt.println("Range set to 2G");
+        break;
+      } else if (range == "4G") {
+        lis.setRange(LIS3DH_RANGE_4_G);
+        Serial.println("Range set to 4G");
+        SerialPortBt.println("Range set to 4G");
+        break;
+      } else if (range == "8G") {
+        lis.setRange(LIS3DH_RANGE_8_G);
+        Serial.println("Range set to 8G");
+        SerialPortBt.println("Range set to 8G");
+        break;
+      } else if (range == "16G") {
+        lis.setRange(LIS3DH_RANGE_16_G);
+        Serial.println("Range set to 16G");
+        SerialPortBt.println("Range set to 16G");
+        break;
+      }
+    }
+  }
+
+
+
+}
+
+
+void configureLISDatarate(){
+   Serial.println("Set datarate to 1HZ, 10HZ, 25HZ, 50HZ, 100Hz, 200HZ, 400HZ, POWERDOWN, 5KHZ, 1K6HZ");
+  SerialPortBt.println("Set datarate to 1HZ, 10HZ, 25HZ, 50HZ, 100Hz, 200HZ, 400HZ, POWERDOWN, 5KHZ, 1K6HZ");
+   while (true) {
+    if (SerialPortBt.available()) {
+      String range = SerialPortBt.readStringUntil('\n');
+      if (range == "1HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_1_HZ);
+        Serial.println("Datarate set to 1HZ");
+        SerialPortBt.println("Datarate set to 1HZ");
+        break;
+      } else if (range == "10HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_10_HZ);
+        Serial.println("Datarate set to 10HZ");
+        SerialPortBt.println("Datarate set to 10HZ");
+        break;
+      } else if (range == "25HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_25_HZ);
+        Serial.println("Datarate set to 25HZ");
+        SerialPortBt.println("Datarate set to 25HZ");
+        break;
+      } else if (range == "50HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+        Serial.println("Datarate set to 50HZ");
+        SerialPortBt.println("Datarate set to 50HZ");
+        break;
+      } else if (range == "100HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_100_HZ);
+        Serial.println("Datarate set to 100HZ");
+        SerialPortBt.println("Datarate set to 100HZ");
+      } else if (range == "200HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_200_HZ);
+        Serial.println("Datarate set to 200HZ");
+        SerialPortBt.println("Datarate set to 200HZ");
+      } else if (range == "400HZ") {
+        lis.setDataRate(LIS3DH_DATARATE_400_HZ);
+        Serial.println("Datarate set to 400HZ");
+        SerialPortBt.println("Datarate set to 400HZ");
+      } else if (range == "POWERDOWN") {
+        lis.setDataRate(LIS3DH_DATARATE_POWERDOWN);
+        Serial.println("Datarate set to POWERDOWN");
+        SerialPortBt.println("Datarate set to POWERDOWN");
+      } else if (range == "5KHZ") {
+        lis.setDataRate(LIS3DH_DATARATE_LOWPOWER_5KHZ);
+        Serial.println("Datarate set to 5KHZ");
+        SerialPortBt.println("Datarate set to 5KHZ");
+      } else if (range == "10K6Z") {
+        lis.setDataRate(LIS3DH_DATARATE_LOWPOWER_1K6HZ);
+        Serial.println("Datarate set to 10K6Z");
+        SerialPortBt.println("Datarate set to 10K6Z");
+      }
+     }  
+    }
+
+
+}
+
+void configureLISPerformance(){
+    Serial.println("Performance mode set to: NORMAL 10bit / LOW Power 8bit / HIGH Resolution 12bit");
+  SerialPortBt.println("Performance mode set to: NORMAL 10bit / LOW Power 8bit / HIGH Resolution 12bit");
+  while (true) {
+    if (SerialPortBt.available()) {
+      String range = SerialPortBt.readStringUntil('\n');
+      if (range == "NORMAL") {
+        lis.setPerformanceMode(LIS3DH_MODE_NORMAL);
+        Serial.println("Set to NORMAL");
+        SerialPortBt.println("Set to NORMAL");
+        break;
+      } else if (range == "LOW") {
+        lis.setPerformanceMode(LIS3DH_MODE_LOW_POWER);
+        Serial.println("Set to LOW");
+        SerialPortBt.println("Set to LOW");
+        break;
+      } else if (range == "HIGH") {
+        lis.setPerformanceMode(LIS3DH_MODE_HIGH_RESOLUTION);
+        Serial.println("Set to HIGH");
+        SerialPortBt.println("Set to HIGH");
+        break;
+      }
+    }
+  }
+}
+
+void printLIS3Configuration(){
+        delay(1000);
+      Serial.println("- - - LIS3DH parameter - - -");
+      SerialPortBt.print("- - - LIS3DH parameter - - -");  
+      // STAMPA delle impostazioni su seriale 
+
+      // lis.setRange(LIS3DH_RANGE_4_G);   // 2, 4, 8 o 16 G!
+      Serial.print("Range = "); 
+      Serial.print(2 << lis.getRange());
+      Serial.println("G");
+      SerialPortBt.print("Range = ");   
+      SerialPortBt.print(2 << lis.getRange());
+      SerialPortBt.println("G");
+
+      // lis.setPerformanceMode(LIS3DH_MODE_LOW_POWER);
+      Serial.print("Performance mode set to: ");
+      SerialPortBt.print("Performance mode set to: ");
+      switch (lis.getPerformanceMode()) {
+        case LIS3DH_MODE_NORMAL: 
+            Serial.println("Normal 10bit"); 
+            SerialPortBt.println("Normal 10bit");        
+            break;
+        case LIS3DH_MODE_LOW_POWER: 
+            Serial.println("Low Power 8bit"); 
+            SerialPortBt.println("Low Power 8bit"); 
+            break;
+        case LIS3DH_MODE_HIGH_RESOLUTION: 
+            Serial.println("High Resolution 12bit"); 
+            SerialPortBt.println("High Resolution 12bit"); 
+            break;
+      }
+      // lis.setDataRate(LIS3DH_DATARATE_50_HZ);
+      Serial.print("Data rate set to: ");
+      SerialPortBt.print("Data rate set to: ");
+      switch (lis.getDataRate()) {
+        case LIS3DH_DATARATE_1_HZ: 
+            Serial.println("1 Hz"); 
+            SerialPortBt.println("1 Hz"); 
+            break;
+        case LIS3DH_DATARATE_10_HZ: 
+            Serial.println("10 Hz"); 
+            SerialPortBt.println("10 Hz"); 
+            break;
+        case LIS3DH_DATARATE_25_HZ: 
+            Serial.println("25 Hz"); 
+            SerialPortBt.println("25 Hz"); 
+            break;
+        case LIS3DH_DATARATE_50_HZ: 
+            Serial.println("50 Hz"); 
+            SerialPortBt.println("50 Hz"); 
+            break;
+        case LIS3DH_DATARATE_100_HZ: 
+            Serial.println("100 Hz"); 
+            SerialPortBt.println("100 Hz"); 
+            break;
+        case LIS3DH_DATARATE_200_HZ: 
+            Serial.println("200 Hz"); 
+            SerialPortBt.println("200 Hz"); 
+            break;
+        case LIS3DH_DATARATE_400_HZ: 
+            Serial.println("400 Hz"); 
+            SerialPortBt.println("400 Hz"); 
+            break;
+        case LIS3DH_DATARATE_POWERDOWN: 
+            Serial.println("Powered Down"); 
+            SerialPortBt.println("Powered Down"); 
+            break;
+        case LIS3DH_DATARATE_LOWPOWER_5KHZ: 
+            Serial.println("5 Khz Low Power"); 
+            SerialPortBt.println("5 Khz Low Power"); 
+            break;
+        case LIS3DH_DATARATE_LOWPOWER_1K6HZ: 
+            Serial.println("1.6 Khz Low Power"); 
+            SerialPortBt.println("1.6 Khz Low Power"); 
+            break;
+       }
 }
